22c8013303956ad0d7ca75656e7b6f4d359b1eb8
Biswo <biswajitdash907@gmail.com>
2023-05-08 01:30:51 +0530

    May-8-2023

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -2,23 +2,37 @@ import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import { Ionicons } from '@expo/vector-icons';
 import { AntDesign } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { colors } from '../Styles/styles';
 
 const Header = ({ back, emptyCart = false }) => {
     const navigate = useNavigation();
+    const route = useRoute();
+
+    const emptyCartHandler = () => {
+        console.log("Emty Cart");
+    }
     return (
         <>
             {
                 back && (
-                    <TouchableOpacity onPress={() => navigate.goBack()}>
-                        <Ionicons name="arrow-back-sharp" size={24} color="black" style={{ position: 'absolute', left: 20, top: 40, zIndex: 10 }} />
+                    <TouchableOpacity onPress={() => navigate.goBack()}
+                        style={{ position: 'absolute', left: 20, top: 40, zIndex: 10 }}
+                    >
+                        <Ionicons name="arrow-back-sharp" size={24}
+                            color={route.name === "productdetails" ? colors.color2 : colors.color3}
+                        />
                     </TouchableOpacity>
                 )
             }
+            <TouchableOpacity onPress={emptyCart ? emptyCartHandler : () => navigate.navigate('cart')}
+                style={{ position: 'absolute', right: 20, top: 40, zIndex: 10 }}
+            >
 
-            <TouchableOpacity onPress={() => navigate.navigate('cart')}>
+                <AntDesign
+                    name={emptyCart ? "antdesign" : "shoppingcart"} size={24}
 
-                <AntDesign name={emptyCart ? "antdesign" : "shoppingcart"} size={24} color="black" style={{ position: 'absolute', right: 20, top: 40, zIndex: 10 }} />
+                    color={route.name === "productdetails" ? colors.color2 : colors.color3} />
             </TouchableOpacity>
 
         </>
--- src/Screen/Home/index.js
+++ src/Screen/Home/index.js
@@ -1,20 +1,59 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
-import { defaultStyle } from '../../Styles/styles'
+import { colors, defaultStyle } from '../../Styles/styles'
 import Header from '../../Component/Header'
+import { AntDesign } from '@expo/vector-icons';
 
 const Home = () => {
+
+    const Categories = ['Shooses', 'Football', 'Man', 'T-Shirts']
+
     return (
         <View style={defaultStyle}>
 
-            {/* Header */}
             <Header />
 
+            {/* Heading Row */}
 
-            <View>
-                <Text style={{ fontSize: 25 }}>Our</Text>
-                <Text style={{ fontSize: 25, fontWeight: '900' }}>Product</Text>
+            <View style={{
+                paddingTop: 70,
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: "center",
+            }}>
+
+
+                {/* Heading */}
+
+                <View>
+                    <Text style={{ fontSize: 25 }}>Our</Text>
+                    <Text style={{ fontSize: 25, fontWeight: '900' }}>Product</Text>
+                </View>
+
+                {/* Serch Bar */}
+                <View>
+                    <TouchableOpacity>
+                        <AntDesign name="search1" size={24} color="black" style={{ elevation: 12, width: 40, height: 40, textAlign: 'center', justifyContent: 'center', borderRadius: 50, backgroundColor: 'red' }} />
+                    </TouchableOpacity>
+                </View>
             </View>
+
+
+            {/* Categories */}
+            {
+
+                Categories.map((item, index) => (
+                    <View style={{
+                        flexDirection: 'row',
+                        height: 80
+                    }}>
+                        <Button title='Nice'></Button>
+                    </View>
+
+                ))
+            }
+
+
         </View>
     )
 }
